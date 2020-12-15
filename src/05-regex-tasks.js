3edf421d355f044d2146473783e3951be818e512
Andrey Znak <a.y.znak@gmail.com>
2020-12-15 13:13:03 +0300

    feat: regex tasks complete

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -79,8 +79,8 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  return new RegExp(`^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{${minLength},}$`);
 }
 
 
